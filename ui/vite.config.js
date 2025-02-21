ba3e5573f9dbf48c06a03b8b0ff8bc373009a06e
LilaKelland <lila.kelland@gmail.com>
2025-02-21 13:55:06 +0000

    added host/port for cloud run in vite.config.js

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,22 +1,42 @@
-import { defineConfig } from 'vite'
+// import { defineConfig, loadEnv } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // // https://vite.dev/config/
+// // export default defineConfig({
+// //   plugins: [react()],
+// // })
+
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, process.cwd());
+
+//   return {
+//     plugins: [react()],
+//     server: {
+//       host: '0.0.0.0', // Required for Cloud Run
+//       port: parseInt(env.VITE_PORT) || 8080, // Default to 8080 for Cloud Run
+//       strictPort: true,
+//     },
+//   };
+// });
+
+
+
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      host: '0.0.0.0', // Required for Cloud Run
+      port: parseInt(env.VITE_PORT) || 8080, // Default to 8080 for Cloud Run
+      strictPort: true,
+    },
+  };
+});
 
 
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       // Proxy all API requests starting with `/api` to your backend
-//       "/api": {
-//         target: "https://3001-cs-281831690367-default.cs-us-east1-yeah.cloudshell.dev",
-//         changeOrigin: true, // Change the origin to the target URL
-//         rewrite: (path) => path.replace(/^\/api/, ""), // Remove `/api` prefix before forwarding
-//       },
-//     },
-//   },
-// });
+
+
