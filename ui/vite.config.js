20be014581bf0f3ae0fc0350444c489b0b608e76
LilaKelland <lila.kelland@gmail.com>
2025-02-21 19:34:23 +0000

    changed port value in vite config to work with a variety of values

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -21,25 +21,8 @@
 
 
 
-// import { defineConfig, loadEnv } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig(({ mode }) => {
-//   const env = loadEnv(mode, process.cwd());
-
-//   return {
-//     plugins: [react()],
-//     server: {
-//       host: '0.0.0.0', // Required for Cloud Run
-//       port: parseInt(env.VITE_PORT) || 8080, // Default to 8080 for Cloud Run
-//       strictPort: true,
-//     },
-//   };
-// });
-
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
@@ -47,21 +30,39 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     server: {
-      host: "0.0.0.0",
-      port: parseInt(env.VITE_PORT) || 8080, // Cloud Run uses 8080
+      host: '0.0.0.0', // Required for Cloud Run
+      port: parseInt(process.env.PORT) || parseInt(env.VITE_PORT) || 8080, // Default to 8080 for Cloud Run
+      // port: isCloudShell ? 8080 : parseInt(env.VITE_PORT) || 5173,
       strictPort: true,
-    },
-    publicDir: "public", // Ensure Vite includes static files
-    build: {
-      outDir: "dist", // Ensure production files go to `dist`
-      rollupOptions: {
-        input: {
-          main: path.resolve(__dirname, "index.html"),
-        },
-      },
     },
   };
 });
+
+// import { defineConfig, loadEnv } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, process.cwd());
+
+//   return {
+//     plugins: [react()],
+//     server: {
+//       host: "0.0.0.0",
+//       port: parseInt(env.VITE_PORT) || 8080, // Cloud Run uses 8080
+//       strictPort: true,
+//     },
+//     publicDir: "public", // Ensure Vite includes static files
+//     build: {
+//       outDir: "dist", // Ensure production files go to `dist`
+//       rollupOptions: {
+//         input: {
+//           main: path.resolve(__dirname, "index.html"),
+//         },
+//       },
+//     },
+//   };
+// });
 
 
 
